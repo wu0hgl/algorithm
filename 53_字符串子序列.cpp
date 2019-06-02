e05c7450c5636f848f063cb9ccb428651390f1d9
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-06-02 10:30:24 +0800

    Add files via upload

---
+++ 53_字符串子序列.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+/*
+    打印一个字符串的全部子序列, 包括空字符串
+*/
+
+void printAllSub(string &str, int i, string &res) {
+    if (i == str.size()) {
+        printf("%s\n", res.c_str());        // 打印之前的收集结构
+        return;
+    }
+    printAllSub(str, i + 1, res);           // 不要当前字符
+    printAllSub(str, i + 1, res + str[i]);  // 要当前字符
+}
+
+
+int main() {
+    string test = "abc";
+    printAllSub(test, 0, string());
+
+    return 0;
+}
