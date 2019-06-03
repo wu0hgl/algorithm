b4897269c6bdcbf60aedbc6519da90cb7c1254d9
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-06-03 08:12:39 +0800

    Add files via upload

---
+++ 55_数组累加和.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+
+
+bool isSum(vector<int> &arr, int i, int sum, int aim) {
+    if (i == arr.size()) {
+        return sum == aim;      // base case
+    }
+    return isSum(arr, i + 1, sum, aim) || isSum(arr, i + 1, sum + arr[i], aim);
+}
+
+int main() {
+    vector<int> arr = { 1, 4, 8 };
+    cout << isSum(arr, 0, 0, 12) << endl;
+
+    return 0;
+}
