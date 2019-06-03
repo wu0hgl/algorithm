11333b2da52ae4aa95a05805013fc493606b35fa
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-06-03 09:03:10 +0800

    Add files via upload

---
+++ 59_逆序栈.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+/*
+    逆序栈, 不能申请额外的数据结构, 只能使用递归函数
+*/
+
+int getAndRemoveLastElement(stack<int> &sk) {
+    int res = sk.top();
+    sk.pop();
+    if (sk.empty()) {       // 获得最后一个元素没有压入栈
+        return res;
+    }
+    else {
+        int last = getAndRemoveLastElement(sk);
+        sk.push(res);       // 恢复栈
+        return last;
+    }
+}
+
+void reverseStack(stack<int> &sk) {
+    if (sk.empty()) {
+        return ;
+    }
+    int i = getAndRemoveLastElement(sk);    // 获得最后一个元素
+    reverseStack(sk);
+    sk.push(i);
+}
+
+int main() {
+    stack<int> sk;
+    for (int i = 0; i < 10; i++)
+        sk.push(i + 1);
+    
+    reverseStack(sk);
+
+    while (!sk.empty()) {
+        cout << sk.top() << " ";
+        sk.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
