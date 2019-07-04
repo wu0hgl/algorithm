f9371070f37b2be340da38afdbfd0fafd50f9894
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-07-04 11:20:19 +0800

    Add files via upload

--- 79_字符串加减乘除.cpp
+++ 79_字符串加减乘除.cpp
@@ -5,18 +5,19 @@
 using namespace std;
 
 /*
-    给定一个字符串str, str表示一个公式, 公式里可能有整数, 加减乘除符号和左右括号, 返回公式的计算结果. 说明:
-        (1)可以认为给定的字符串一定是正确的公式， 即不需要对str做公式有效性检查。
-        (2)如果是负数, 就需要用括号括起来, 比如`4*(-3)`. 但如果负数作为公式的开头或括号部分的开头, 则可以没有括号, 比如`-3*4`和`(-3*4)`都是合法的。
-        (3)不用考虑计算过程中会发生溢出的情况
+    给定一个字符串str, str表示一个公式, 公式里可能有整数, 加减乘除符号和左右括号, 返回公式的计算结果. 
+    说明:
+    (1)可以认为给定的字符串一定是正确的公式, 即不需要对str做公式有效性检查
+    (2)如果是负数, 就需要用括号括起来, 比如`4*(-3)`. 但如果负数作为公式的开头或括号部分的开头, 则可以没有括号, 比如`-3*4`和`(-3*4)`都是合法的
+    (3)不用考虑计算过程中会发生溢出的情况
 */
 
-void addNum(list<string>& que, int num) {
+void addNum(list<string>& que, int num) {           // *, /操作结算
     if (!que.empty()) {
         int cur = 0;
         string top = que.back(); que.pop_back();
         if ((top == "+") || (top == "-")) {         // +, -不操作
-            que.push_back(top);                     // 把弹出的字符串再压入list中
+            que.push_back(top);
         }
         else {                                      // *, / 计算
             cur = stoi(que.back(), nullptr, 10);    // 再取出一个数字进行计算
@@ -27,7 +28,7 @@ void addNum(list<string>& que, int num) {
     que.push_back(to_string(num));
 }
 
-int getNum(list<string>& que) {     // 结算收集到的数字
+int getNum(list<string>& que) {                     // 结算list中+, -表达式
     int res = 0;
     bool add = true;
     string cur;
@@ -53,17 +54,17 @@ vector<int> value(string str, size_t i) {
     int pre = 0;
     vector<int> bra;
     while ((i < str.size()) && (str[i] != ')')) {
-        if ((str[i] >= '0') && (str[i] <= '9')) {     // 收集数字
+        if ((str[i] >= '0') && (str[i] <= '9')) {   // 收集数字
             pre = pre * 10 + (str[i++] - '0');
         }
         else if (str[i] != '(') {                   // 遇到运算符号进行计算(+ , -, *, /)
             addNum(que, pre);
 
-            string temp;
+            string temp;                            // 把char处理成string
             temp += str[i++];
             que.push_back(temp);
 
-            pre = 0;    // 计算之后把收集的数字置0
+            pre = 0;                                // 计算之后把收集的数字置0
         }
         else {
             bra = value(str, i + 1);                // 遇到左括号
@@ -72,11 +73,9 @@ vector<int> value(string str, size_t i) {
         }
     }
     addNum(que, pre);
-
     vector<int> ret;
     ret.push_back(getNum(que));
     ret.push_back(i);
-
     return ret;
 }
 
@@ -85,10 +84,21 @@ int getValue(string& str) {
 }
 
 int main() {
-    string exp = " 48 *((70-65)-43)+8*1";
+    string exp = "48*((70-65)-43)+8*1";
     //string exp = "12*((11-12)-21)+1*2";
     cout << getValue(exp) << endl;
 
+    exp = "4*(6+78)+53-9/2+45*8";
+    cout << getValue(exp) << endl;
+
+    exp = "10-5*3";
+    cout << getValue(exp) << endl;
+
+    exp = "-3*4";
+    cout << getValue(exp) << endl;
+
+    exp = "3+1*4";
+    cout << getValue(exp) << endl;
 
     return 0;
 }
