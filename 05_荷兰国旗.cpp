3e1ddd4cc498ac6b920839d12133e2f961f27bda
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-07-10 11:37:24 +0800

    Add files via upload

--- 05_荷兰国旗.cpp
+++ 05_荷兰国旗.cpp
@@ -3,12 +3,44 @@
 #include <vector>
 #include <ctime>
 using namespace std;
+void generatorRandomArray(vector<int> &vt, int maxSize, int maxValue);
+void printVector(vector<int> &vt);
 
 /*
-给定一个数组arr, 和一个数num,
-请把小于num的数放在数组的左边, 等于num的数放在数组的中间, 大于num的数放在数组的右边
-要求额外空间复杂度O(1), 时间复杂度O(N)
+    给定一个数组arr, 和一个数num,
+    请把小于num的数放在数组的左边, 等于num的数放在数组的中间, 大于num的数放在数组的右边
+    要求额外空间复杂度O(1), 时间复杂度O(N)
 */
+
+void func(vector<int> &arr, int left, int right, int aim) {
+    int rightPtr = right + 1;   // 当前所指的位置就是大于aim数的左边界
+    int leftPtr = left - 1;     // 当前所指的位置就是小于aim数的右边界
+    int index = left;
+    while (index < rightPtr) {  // 这里不能有等于号, 因为rightPtr所指位置就是大于aim的左边界
+        if (arr[index] < aim) {
+            swap(arr[++leftPtr], arr[index++]);
+        }
+        else if (arr[index] > aim) {
+            swap(arr[index], arr[--rightPtr]);  // 从右边界交换过来的数组还未与aim比较, 所以index不变
+        }
+        else {
+            index++;
+        }
+        //printVector(arr);
+    }
+}
+
+int main() {
+    vector<int> arr;
+    generatorRandomArray(arr, 30, 10);
+    //vector<int> arr = { 8, 2, 4 };
+    printVector(arr);
+
+    func(arr, 0, arr.size() - 1, 4);
+    printVector(arr);
+
+    return 0;
+}
 
 void printVector(vector<int> &vt) {
     for (auto v : vt)
@@ -16,32 +48,19 @@ void printVector(vector<int> &vt) {
     cout << endl;
 }
 
-void func(vector<int> &arr, int left, int right, int num) {
-    int rightPtr = right + 1;
-    int leftPtr = left - 1;
-    int index = left;
-    while (index < rightPtr) {
-        if (arr[index] < num) {
-            swap(arr[++leftPtr], arr[index++]);
-        }
-        else if (arr[index] > num) {
-            swap(arr[index], arr[--rightPtr]);
+void generatorRandomArray(vector<int> &vt, int maxSize, int maxValue) {
+    int temp;
+    int len = (int)(maxSize % (maxSize + 1));
+
+    srand(static_cast<int>(time(NULL)));
+    for (int i = 0; i < len; i++) {
+        temp = static_cast<int>(rand() % (maxValue + 1)) - static_cast<int>(rand() % (maxValue));   // 随机生成正负数
+        //temp = static_cast<int>(rand() % (maxValue + 1));                                         // 随机生成正数
+        if (temp == 0) {
+            continue;
         }
         else {
-            index++;
+            vt.push_back(temp);
         }
     }
-}
-
-int main() {
-    vector<int> arr;
-    srand(static_cast<int>(time(NULL)));
-    for (int i = 0; i < 20; i++) {
-        arr.push_back(static_cast<int>(rand() % 10));
-    }
-    printVector(arr);
-    func(arr, 0, arr.size() - 1, 4);
-    printVector(arr);
-
-    return 0;
 }
