36f019e22a5b4d9a3d250f966548031199741774
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-07-12 08:30:16 +0800

    Add files via upload

--- 11_堆排.cpp
+++ 11_堆排.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <vector>
 #include <stack>
 #include <algorithm>
@@ -9,17 +9,17 @@ void generatorRandomArray(vector<int> &vt, int maxSize, int maxValue);
 void printVector(vector<int> &vt);
 
 /*
-    ¶ÑÅÅ
+    å †æ’
 */
 
-void heapInsert(vector<int> &arr, int index) {      // ÉÏ¸¡: µ±Ç°½ÚµãÈô±È¸¸½Úµã´ó, ½»»»Á½¸ö½Úµã
-    while (arr[index] > arr[(index - 1) / 2]) {     // (0 - 1) / 2 = 0»¹ÊÇºÍ0±È½Ï
+/* æœ€å¤§å † */
+void heapInsert_MAX(vector<int> &arr, int index) {      // ä¸Šæµ®: å½“å‰èŠ‚ç‚¹è‹¥æ¯”çˆ¶èŠ‚ç‚¹å¤§, äº¤æ¢ä¸¤ä¸ªèŠ‚ç‚¹
+    while (arr[index] > arr[(index - 1) / 2]) {     // å¾ªç¯è¾¹ç•Œæ¡ä»¶æŒºæœ‰æ„æ€, (0 - 1) / 2 = 0è¿˜æ˜¯å’Œ0æ¯”è¾ƒ
         swap(arr[index], arr[(index - 1) / 2]);
         index = (index - 1) / 2;
     }
 }
-
-void heapify(vector<int> &vt, int index, int size) {    // ÏÂ³Á: µ±Ç°½Úµã±È×Ó½ÚµãĞ¡
+void heapify_MAX(vector<int> &vt, int index, int size) {    // ä¸‹æ²‰: å½“å‰èŠ‚ç‚¹æ¯”å­èŠ‚ç‚¹å°
     int left = index * 2 + 1;
     while (left < size) {
         int largest = (left + 1 < size && vt[left + 1] > vt[left])
@@ -33,27 +33,65 @@ void heapify(vector<int> &vt, int index, int size) {    // ÏÂ³Á: µ±Ç°½Úµã±È×Ó½Úµ
         left = 2 * index + 1;
     }
 }
-
-void heapSort(vector<int> &vt) {
+void heapSort_MIN_MAX(vector<int> &vt) {
     if (vt.size() < 2)
         return;
-    for (int i = 0; i < vt.size(); i++) {
-        heapInsert(vt, i);
+    for (int i = 0; i < static_cast<int>(vt.size()); i++) {
+        heapInsert_MAX(vt, i);
     }
     int size = vt.size();
     swap(vt[0], vt[--size]);
     while (size > 0) {
-        heapify(vt, 0, size);
+        heapify_MAX(vt, 0, size);
         swap(vt[0], vt[--size]);
     }
 }
 
+/* æœ€å°å † */
+void heapify_MIN(vector<int>& arr, int index) {
+    while (arr[index] < arr[(index - 1) / 2]) {
+        swap(arr[index], arr[(index - 1) / 2]);
+        index = (index - 1) / 2;
+    }
+}
+void heapify_MIN(vector<int>& arr, int index, int len) {
+    int lPtr = index * 2 + 1;
+    while (lPtr < len) {
+        int least = (lPtr + 1 < len && arr[lPtr + 1] < arr[lPtr]) 
+                    ? lPtr + 1
+                    : lPtr;
+        least = arr[index] < arr[least] ? index : least;
+        if (least == index) {
+            return;
+        }
+        swap(arr[least], arr[index]);
+        index = least;
+        lPtr = index * 2 + 1;
+    }
+}
+void heapSort_MAX_MIN(vector<int>& arr) {
+    if (arr.size() < 2)
+        return;
+    for (int i = 0; i < static_cast<int>(arr.size()); i++) {
+        heapify_MIN(arr, i);
+    }
+    int size = arr.size();
+    swap(arr[0], arr[size - 1]);
+    for (int i = size - 1; i > 0; i--) {
+        heapify_MIN(arr, 0, i);
+        swap(arr[0], arr[i - 1]);
+    }
+}
+
 int main() {
+    //vector<int> arr = { 1, 2 };
     vector<int> arr;
     srand(static_cast<int>(time(NULL)));
     generatorRandomArray(arr, 20, 200);
     printVector(arr);
-    heapSort(arr);
+    heapSort_MIN_MAX(arr);
+    printVector(arr);
+    heapSort_MAX_MIN(arr);
     printVector(arr);
 
     return 0;
@@ -70,8 +108,8 @@ void generatorRandomArray(vector<int> &vt, int maxSize, int maxValue) {
     int len = (int)(maxSize % (maxSize + 1));
 
     for (int i = 0; i < len; i++) {
-        temp = static_cast<int>(rand() % (maxValue + 1)) - static_cast<int>(rand() % (maxValue));   // Ëæ»úÉú³ÉÕı¸ºÊı
-        //temp = static_cast<int>(rand() % (range + 1));          // Ëæ»úÉú³ÉÕıÊı
+        temp = static_cast<int>(rand() % (maxValue + 1)) - static_cast<int>(rand() % (maxValue));   // éšæœºç”Ÿæˆæ­£è´Ÿæ•°
+        //temp = static_cast<int>(rand() % (range + 1));          // éšæœºç”Ÿæˆæ­£æ•°
         vt.push_back(temp);
     }
 }
