4fd272afb7e897fbfa8f2c424b1fec072230f0c8
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-07-25 12:15:48 +0800

    Add files via upload

---
+++ 95_给定字符串构成最长回文字符串.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+// 使用stl中的哈希表
+int longestPalindrome_1(string str) {
+    unordered_map<char, int> myMap;
+
+    for (size_t i = 0; i < str.size(); i++) {
+        myMap[str[i]]++;
+    }
+    int res = 0;
+    for (auto i : myMap) {
+        res += (i.second / 2) * 2;
+    }
+    if (res < str.size()) {
+        res++;
+    }
+
+    return res;
+}
+
+// 通过ASCII构造哈希表
+int longestPalindrome_2(string str) {
+    vector<int> cnts(58);
+    for (size_t i = 0; i < str.size(); i++) {
+        cnts[str[i] - 'A']++;
+    }
+    size_t res = 0;
+    for (auto cnt : cnts) {
+        res += (cnt / 2) * 2;
+    }
+
+    if (res < str.size()) {
+        res++;
+    }
+    return res;
+}
+
+int main(void)
+{
+    string str = "abccccdd";
+
+    cout << longestPalindrome_1(str) << endl;
+    cout << longestPalindrome_2(str) << endl;
+
+    return 0;
+}
