da0ecc13322390693bddaa09d0ff2c68f02b1f7e
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-08-11 10:13:32 -0500

    Add files via upload

---
+++ 106_最大公倍数.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+    最大公约数/最小公倍数
+*/
+
+int func(int a, int b){
+
+    if (b == 0) {
+        return a;
+    }
+
+    return maxyue(b, a%b);
+}
+
+int main(){
+    int a, b;
+    while (cin >> a >> b){
+        cout << a * b / func(a, b) << endl;
+    }
+    return 0;
+}
