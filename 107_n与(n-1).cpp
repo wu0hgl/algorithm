0f195a7880b052d7daccd7c6cacb10d641fd9a71
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-08-12 03:17:29 -0500

    Add files via upload

---
+++ 107_n与(n-1).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void func_1() {         // 判断一个数是否是2的幂
+    int n, count = 0;
+    cin >> n;
+    if ((n > 0) && (!(n & (n - 1))))
+        cout << "这个数是2的幂" << endl;
+    else
+        cout << "这个数不是2的幂" << endl;
+}
+
+void func_2() {         // 统计一个数二进制表达式中1的个数
+    int n, count = 0;
+    cin >> n;
+    while (n > 0)
+    {
+        count++;
+        n &= (n - 1);
+    }
+    cout << count << endl;
+}
+
+int main()
+{
+    func_1();
+    func_2();
+    return 0;
+}
