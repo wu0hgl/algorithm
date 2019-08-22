e091c9252c79c1c44ce47ca3ef6c410fc2bf0c02
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-08-22 02:53:02 -0500

    Add files via upload

---
+++ 110_最长对称字符串.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+给定的字符串里面, 找出最长的对称字符串(可以跳过部分字符)
+输入: 字符串
+输出: 最长字符串的长度
+abcba   --> 5
+abcdeba --> 5 (abcba与abcba)
+aeaabab --> 4 (aaaa与aaaa对称)
+*/
+
+int max = 0;
+
+void func(string& s, int begin, int end, int len){
+    if (begin > end) return;
+    if (begin == end){
+        len += 1;
+        if (len > max){
+            max = len;
+        }
+        return;
+    }
+    if (str[begin] == str[end]){
+        if (len + 2 > max){
+            max = len + 2;
+        }
+        func(str, begin + 1, end - 1, len + 2);
+    }
+    func(str, begin + 1, end, len);
+    func(str, begin, end - 1, len);
+}
+
+int main() {
+    string str = "abcba";
+    func(str, 0, str.size() - 1, 0);
+    cout << max << endl;
+
+    max = 0;
+    str = "abcdeba";
+    func(str, 0, str.size() - 1, 0);
+    cout << max << endl;
+
+    max = 0;
+    str = "aeaabab";
+    func(str, 0, str.size() - 1, 0);
+    cout << max << endl;
+}
