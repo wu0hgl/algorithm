4083b2cca16536792f7d832cb5786fa5c9f88f3d
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-08-29 20:50:06 -0500

    Add files via upload

--- 110_strlen.cpp
+++ 110_strlen.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <assert.h>
 using namespace std;
 
 /* 不用中间变量，用递归实现，很容易看懂 */
@@ -11,7 +12,7 @@ int strlen_1(const char *str) {
 
 /* 不用中间变量，也是用递归实现，写得更简洁而已 */
 int strlen_2(const char *str) {
-    return *str ? (strlen(++str) + 1) : 0;
+    return *str ? (strlen_2(++str) + 1) : 0;
 }
 
 int strlen_3(const char * str) {
@@ -22,8 +23,31 @@ int strlen_3(const char * str) {
     return(eos - str - 1);
 }
 
+char* strcat(char* des, const char* src) {   // const表明为输入参数 
+    assert((des != NULL) && (src != NULL));
+    char* address = des;
+    while (*des != '\0')  // 移动到字符串末尾
+        ++des;
+    while (*des++ = *src++)
+        ;
+    return address;
+}
+
+int strcmp(const char *s1, const char *s2) {
+    assert((s1 != NULL) && (s2 != NULL));
+    while (*s1 == *s2)
+    {
+        if (*s1 == '\0')
+            return 0;
+
+        ++s1;
+        ++s2;
+    }
+    return *s1 - *s2;
+}
+
 int main() {
-    const char str[] = " ";
+    const char str[] = "asd ";
     cout << strlen(str) << endl;;
     cout << strlen_1(str) << endl;;
     cout << strlen_2(str) << endl;;
