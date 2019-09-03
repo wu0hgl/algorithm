e4cd4e1543fb6e7c217a51fbfe5e5b4fde874878
wu0hgl <32703511+wu0hgl@users.noreply.github.com>
2019-09-02 20:56:18 -0500

    Add files via upload

---
+++ 112_重载++--.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+using namespace std;
+class Test {
+    friend Test& operator--(Test &obj);
+    friend Test operator--(Test &obj, int);
+public:
+    Test(int a = 0, int b = 0) {
+        this->a = a;
+        this->b = b;
+    }
+    void display() {
+        cout << "a:" << a << " b:" << b << endl;
+    }
+public:
+    Test & operator++() {   // 前置++
+        this->a++;
+        this->b++;
+        return *this;
+    }
+    Test operator++(int) {  // 后置++
+        Test temp = *this;
+        this->a++;
+        this->b++;
+        return temp;
+    }
+private:
+    int a;
+    int b;
+};
+
+Test& operator--(Test &obj) {      // 前置--
+    obj.a--;
+    obj.b--;
+    return obj;
+}
+
+Test operator--(Test &obj, int) {   //后置--
+    Test temp = obj;
+    obj.a--;
+    obj.b--;
+    return temp;
+}
+
+int main() {
+    Test t1(1, 2);
+    t1.display();
+    ++t1;
+    t1.display();
+    --t1;
+    t1.display();
+    Test t2(3, 4);
+    t2.display();
+    t2++;
+    t2.display();
+    t2--;
+    t2.display();
+
+    return 0;
+}
